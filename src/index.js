628356209e8e853d082e5f96bd8c104adb251e43
Viktoriia-Romanenko <112563877+Viktoriia-Romanenko@users.noreply.github.com>
2022-09-17 21:47:34 +0300

    Rounded min and max temperature, made relevant icons for each week day, formatted each number of day in week to relevant name, displayed only 6 columns of days insteed of 8

--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row week-container">`;
   let days = ["Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `<div class="col-2 borders-settings">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2 borders-settings">
                         
-                        <div class="weather-forecast-date">${day}</div>
+                        <div class="weather-forecast-date">${formatDay(
+                          forecastDay.dt
+                        )}</div>
                         <div class="card">
-                          <img src="https://openweathermap.org/img/wn/50d@2x.png" class="responsive" alt="cloud-rain">
+                          <img src="https://openweathermap.org/img/wn/${
+                            forecastDay.weather[0].icon
+                          }@2x.png" class="responsive" alt="" width="42"/>
                           <div class="card-body">
                           </div>
                        
                           <div class="weather-forecast-temperatures">
                           <span class="weather-forecast-temperature-max">
-                          18°
+                          ${Math.round(forecastDay.temp.max)}°
                         </span>
                         <span class="weather-forecast-temperature-min">
-                          12°
+                          ${Math.round(forecastDay.temp.min)}°
                         </span>
                         </div>
                        
                       </div>
                       </div>`;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
